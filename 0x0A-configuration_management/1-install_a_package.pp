23a08425b22d63b170685847e869db592afc8250
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-12-22 06:32:50 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,5 +3,12 @@
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure   => 'installed',
+}
+
+package { 'werkzeug':
+  ensure   => 'installed',
 }
