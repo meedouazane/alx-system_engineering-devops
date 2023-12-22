e6e53fdbe2775e5ad684131ec4379ab8c53f5af8
Mohamed ouazane <mohamedouazane@yahoo.com>
2023-12-22 07:05:33 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,23 @@
 # install flask from pip and ensure that depancances are installed
 
+# ensure that python present
 package { 'python3':
   ensure   => present,
 }
 
+# ensure that pip present
 package { 'python3-pip':
   ensure   => present,
 }
 
+# install flask 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
   require  => Package['python3-pip'],
 }
 
+# install werkzeug
 package { 'werkzeug':
   ensure   => '2.1.0',
   provider => 'pip',
