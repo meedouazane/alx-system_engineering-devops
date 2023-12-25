26ad2d63f30b4e1e89bd4846718386c3a1de0164
meedouazane <mohamedouazane@yahoo.com>
2023-12-25 15:05:50 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Ensure the existence of the SSH config file
 
-file { '/root/.ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
 }
 file_line { 'Declare identity file':
-  path  => '/root/.ssh/ssh_config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off passwd auth':
-  path  => '/root/.ssh/ssh_config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
 }
