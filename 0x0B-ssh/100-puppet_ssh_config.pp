52779cc2b09784fd9d612888e105c343137683ba
meedouazane <mohamedouazane@yahoo.com>
2023-12-25 10:36:15 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,23 @@
 # Client configuration file (w/ Puppet)
 
 file { '/root/.ssh/ssh_config':
-  ensure  => present,
+  ensure => present,
 }
 
 file_line { 'Client configuration file':
-  path => '/root/.ssh/ssh_config',
-  line => 'Host *',
+  path  => '/root/.ssh/ssh_config',
+  line  => 'Host *',
+  match => '^#?Host \*$',
 }
 
 file_line { 'Client configuration file 2':
-  path => '/root/.ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
+  path  => '/root/.ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile ~/.ssh/school$',
 }
 
 file_line { 'Client configuration file 3':
-  path => '/root/.ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+  path  => '/root/.ssh/ssh_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication no$',
 }
