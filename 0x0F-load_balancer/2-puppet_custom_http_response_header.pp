b0c5ffc2e12a0719b686082f9e131d4464f8931a
meedouazane <mohamedouazane@yahoo.com>
2024-01-08 07:36:33 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,16 +7,22 @@ package { 'haproxy':
   provider => apt,
 }
 
-# Create the HAProxy configuration file
-file { '/etc/haproxy/haproxy.cfg':
-  ensure  => file,
-  content => 'frontend http-in
+# the HAProxy configuration file
+file_line { 'append to /etc/haproxy/haproxy.cfg':
+  path   => '/etc/haproxy/haproxy.cfg',
+  line   => 'frontend http-in
     bind *:80
     default_backend servers
 
 backend servers
     balance roundrobin
     server 429410-web-01 54.167.181.186:80 check
-    server 429410-web-02 35.153.194.155:80 check',
-  notify  => Service['haproxy'],
+    server 429410-web-02 35.153.194.155:80 check
+',
+  notify => Exec['restart_haproxy'],
+}
+
+exec { 'restart_haproxy':
+    command => 'sudo service haproxy restart',
+    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
