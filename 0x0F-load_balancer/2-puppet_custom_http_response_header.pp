fcb6e1650d3a5cd375cca00e5d84aa45da8191c7
meedouazane <mohamedouazane@yahoo.com>
2024-01-08 11:38:57 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 #Install Nginx web server
-exec { 'update server':
-  command  => 'apt-get update',
-  user     => 'root',
-  provider => 'shell',
-}
+
 #install nginx
 package {'nginx':
   ensure   => 'installed',
   provider => apt,
 }
-file_line { 'Add a custom HTTP header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;'
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => '
+server {
+        listen 80 ;
+        listen [::]:80;
+        root /var/www/html;
+        index index.html;
+        server_name _;
+        location / {
+                try_files $uri $uri/ =404;
+                add_header X-Served-By \$hostname always;
+        }
+}',
+  notify  => Service['nginx'],
 }
 service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
 }
+
