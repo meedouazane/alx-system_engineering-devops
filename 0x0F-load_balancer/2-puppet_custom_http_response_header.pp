ca3f4f7a953398a6ce5fa11b1cb6e6c7f7fe43a8
meedouazane <mohamedouazane@yahoo.com>
2024-01-08 10:21:39 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -19,10 +19,4 @@ backend servers
     server 429410-web-01 54.167.181.186:80 check
     server 429410-web-02 35.153.194.155:80 check
 ',
-  notify => Exec['restart_haproxy'],
-}
-
-exec { 'restart_haproxy':
-    command => 'sudo service haproxy restart',
-    path    => '/usr/bin:/usr/sbin:/bin:/sbin',
 }
