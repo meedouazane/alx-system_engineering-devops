70d6a916bf99568bab25624df0089e8059d15d2f
meedouazane <mohamedouazane@yahoo.com>
2024-01-08 06:22:05 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,22 @@
-#!/usr/bin/env bash
-#Install Nginx web server
+#!/usr/bin/env puppet
+# Install HAProxy and configure it
 
-#install haproxy
-package {'haproxy':
+# Install haproxy
+package { 'haproxy':
   ensure   => 'installed',
   provider => apt,
 }
-file {'/etc/haproxy/haproxy.cfg':
+
+# Create the HAProxy configuration file
+file { '/etc/haproxy/haproxy.cfg':
   ensure  => file,
   content => 'frontend http-in
     bind *:80
-    default_backend servers',
-}
-file {'/etc/haproxy/haproxy.cfg':
-  ensure  => file,
-  content => 'backend servers
+    default_backend servers
+
+backend servers
     balance roundrobin
     server 429410-web-01 54.167.181.186:80 check
     server 429410-web-02 35.153.194.155:80 check',
+  notify  => Service['haproxy'],
 }
