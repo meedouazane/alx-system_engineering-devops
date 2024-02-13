d6d8718da4bf699e5a55f50b1d3f12d97eefff06
Ouazane <mohamedouazane@yahoo.com>
2024-02-13 03:32:21 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,12 @@
 """
 queries the Reddit API and returns the number of subscribers
 """
+import requests
 
 
 def number_of_subscribers(subreddit):
     """ Get number of subscribers """
-    import requests
-
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     r = requests.get(url, headers={"User-Agent": "subscribers/1.0"},
                      allow_redirects=False)
     if r.status_code <= 300:
