f0143a57f904839eabd2ba12fa67910a1b47e432
Ouazane <mohamedouazane@yahoo.com>
2024-02-13 04:10:49 -0800

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and returns top 10 hot
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """  prints the titles of the first 10 hot posts """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    r = requests.get(url, headers={"User-Agent": "subscribers/1.0"},
+                     allow_redirects=False)
+    if r.status_code >= 300:
+        return 0
+    response = r.json()
+    for i in range(0, 10):
+        print(response['data']['children'][i]['data']['title'])
