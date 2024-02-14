aecac3e308e56cc44ca8aa4ca6d76b41690ea07a
Ouazane <mohamedouazane@yahoo.com>
2024-02-14 05:10:35 -0800

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,5 @@
 # fixing error and automate it using Puppet
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => "
-<VirtualHost *:80>
-        ServerName localhost
-        ServerAdmin webmaster@localhost
-        DocumentRoot  /var/www/html/wp-content/
-        ErrorLog \${APACHE_LOG_DIR}/error.log
-        CustomLog \${APACHE_LOG_DIR}/access.log combined
-</VirtualHost>",
-  notify  => Exec['restart_apache'],
-}
-
-exec { 'restart_apache':
-  command     => '/etc/init.d/apache2 restart',
-  refreshonly => true,
+exec { 'fixing-php':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
